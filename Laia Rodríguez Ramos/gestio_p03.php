9456ad8d20e97fd0e240454760a595ab3f1ac976
LaiaRodri <laia.rodriguez@enti.cat>
2023-10-10 19:11:47 +0200

    Comentaris

---
+++ Laia Rodríguez Ramos/gestio_p03.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+        //VARIABLES NECESSARIES DE POST AMB EL FORMULARI
+        $id = $_POST["id"];
+        $nomUser = $_POST["nom"];
+        $cognomUser = $_POST["cognom"];
+        $password = $_POST["password"];
+        $email = $_POST["email"];
+
+        echo "hola " .$nomUser;
+    
+    ?>
+</body>
+</html>
