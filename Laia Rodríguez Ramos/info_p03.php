2e9e6bbd5bf19a12702c89a5b6076931d37ae3bd
LaiaRodri <laia.rodriguez@enti.cat>
2023-10-15 13:32:24 +0200

    Fixed double POST. Delete the ID POST into form user. Done.

--- Laia Rodríguez Ramos/info_p03.php
+++ Laia Rodríguez Ramos/info_p03.php
@@ -3,12 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Resultat Laia Rodríguez Ramos</title>
 </head>
 <body>
-    <?php
-
-        mysqli_close($connect);
-    ?>
+    <p>S'ha guardat l'usuari correctament.</p>
 </body>
 </html>
