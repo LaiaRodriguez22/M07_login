c5205633e74d933122003a7cbbd9b2908d5a6bff
LaiaRodri <laia.rodriguez@enti.cat>
2023-10-21 12:15:58 +0200

    Pensant en funcions per alumne i professor

--- Laia Rodríguez Ramos/logUser_p05.php
+++ Laia Rodríguez Ramos/logUser_p05.php
@@ -9,6 +9,9 @@
     <?php
         
         include "dbConf.php";
+
+        $connect =  mysqli_connect(DB_HOST,DB_USER,DB_PSW,DB_NAME);
+
         /*
         define("DB_HOST", "localhost");
         define("DB_NAME", "users");
@@ -20,13 +23,30 @@
         $password = $_POST["password"];
         $email = $_POST["email"];
 
+        $role1 = 'Professor';
+        $role2 = 'Alumne';
+
+        $result = mysqli_query($connect, $query);
+
         //try catch i finally
-        $connect =  mysqli_connect(DB_HOST,DB_USER,DB_PSW,DB_NAME);
+        try {
+            if($connect){
+                $query = "SELECT FROM userlaia ( password, email) VALUES ('$password', '$email')";
 
-        $query = "INSERT INTO userlaia ( password, email) VALUES ('$password', '$email')";
+                if(isset($_POST["email"]) && isset($_POST["password"])){
+                    
+                } else echo "Els valors son incorrectes.";
 
+            }
+            else echo "Els valors IDK.";
+        }
+        catch (Exception $e) {
+            echo "No m'he pogut conectar a la base de dades. "  .$e->getMessage();
+        }
+        finally{
+            mysqli_close($connect);
+        }
         
-
     ?>
 </body>
 </html>
