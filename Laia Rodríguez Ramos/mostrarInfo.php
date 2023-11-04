c1f58a3bb063b5e03259b23b4ad60c0dddc5014b
LaiaRodri <laia.rodriguez@enti.cat>
2023-11-04 12:13:19 +0100

    Fixed mostrarInfo cookie default.

--- Laia Rodríguez Ramos/mostrarInfo.php
+++ Laia Rodríguez Ramos/mostrarInfo.php
@@ -13,20 +13,51 @@
 </head>
 <body>
     <?php
-
         if (!isset($_COOKIE[$cookieLang])) {
             //echo 'La cookie es la default. CAT.';
-            setcookie($cookieLang, $cookieCat);
-            cookieShowInfoCat();
+            //setcookie($cookieLang, $cookieCat, $tempsDuracio);
+            //AQUI AL FER EL SETCOOKIE, ES FEIA EL BUCLE AMB DELETE I PER AIXO
+            //LA COOKIE NO S'ESBORRABA MAI.
+            cookieShowInfoDefault();
         } else if(isset($_COOKIE[$cookieLang])){
             if($_COOKIE[$cookieLang] == "es"){
-                cookieShowInfoEs();
+                cookieValueEs();
             }
             else if($_COOKIE[$cookieLang] == "cat"){
-                cookieShowInfoCat();
+                cookieValueCat();
             }
             else if($_COOKIE[$cookieLang] == "en"){
-                cookieShowInfoEn();
+                cookieValueEn();
+            }
+        }
+
+        //DEFAULT
+        function cookieShowInfoDefault() {
+            if (isset($_GET["id"])) {
+                $user_id = $_GET["id"];
+        
+                $connect = mysqli_connect(DB_HOST, DB_USER, DB_PSW, DB_NAME);
+                $query = "SELECT * FROM userlaia WHERE user_id = '$user_id'";
+        
+                $result = mysqli_query($connect, $query);
+        
+                if ($result && mysqli_num_rows($result) > 0) {
+                    $user = mysqli_fetch_assoc($result);
+                    echo "<h1>Informació detallada de l'usuari:</h1><br>";
+                    echo "<b>Nom:</b> " . $user['username'] . "<br>";
+                    echo "<b>Cognom:</b> " . $user['surname'] . "<br>";
+                    echo "<b>Email:</b> " . $user['email'] . "<br>";
+                    echo "<b>Rol:</b> " . $user['rol'] . "<br>";
+                    echo "<b>Actiu: </b>" . $user['active'] . "<br><br>";
+                } else {
+                    echo "Usuari no trobat o sense permisos per accedir a aquesta informació.";
+                    header('Location: index.php');
+                }
+                echo '<a href="index.php">Tornar</a>';
+            } else {
+                echo "ID de l'usuari no proporcionat.";
+                // TORNEM AL LOGIN
+                header('Location: logUser.html');
             }
         }
 
