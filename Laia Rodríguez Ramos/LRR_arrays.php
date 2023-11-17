e703de7cc336507133ba10ae66b7aad581531443
LaiaRodri <laia.rodriguez@enti.cat>
2023-11-17 16:16:15 +0100

    Taula restaurant.

--- Laia Rodríguez Ramos/LRR_arrays.php
+++ Laia Rodríguez Ramos/LRR_arrays.php
@@ -13,8 +13,18 @@
         
         for ($i = 0; $i < 10; $i++) {
             $taulesRestaurant[$i] = rand(0, 5);
-            echo $taulesRestaurant[$i] . "<br>";
+            echo "La taula ". $i ." té ". $taulesRestaurant[$i] . " comensals.<br>";
+            if($taulesRestaurant[$i] == 0){
+                echo "BUIDA.<br>";
+            }
+            elseif($taulesRestaurant[$i] == 5){
+                echo "PLENA.<br>";
+            }
+            else{
+                echo "té X comensals.<br>";
+            }
         }
+
 
 
 
