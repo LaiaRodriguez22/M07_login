1c9ac6e5c029fd694a222a53934df7b801588f5b
LaiaRodri <laia.rodriguez@enti.cat>
2023-11-17 16:49:18 +0100

    Acabat. Fer README.

--- Laia Rodríguez Ramos/LRR_arrays.php
+++ Laia Rodríguez Ramos/LRR_arrays.php
@@ -7,21 +7,44 @@
 </head>
 <body>
     <?php
-        echo "<h1>Taules del restaurant</h1>";
+        echo "<h1>RESTAURANT</h1>";
 
         $taulesRestaurant = array();
         
-        for ($i = 0; $i < 10; $i++) {
-            $taulesRestaurant[$i] = rand(0, 5);
-            echo "La taula ". $i ." té ". $taulesRestaurant[$i] . " comensals.<br>";
-            if($taulesRestaurant[$i] == 0){
-                echo "BUIDA.<br>";
+        for ($taula = 0; $taula < 10; $taula++) {
+            $taulesRestaurant[$taula] = rand(0, 5);
+            //echo "La taula ". $taula ." té ". $taulesRestaurant[$taula] . " comensals.<br>";
+            if($taulesRestaurant[$taula] == 0){
+                echo "La taula ". $taula ." està buida.<br>";
             }
-            elseif($taulesRestaurant[$i] == 5){
-                echo "PLENA.<br>";
+            elseif($taulesRestaurant[$taula] == 5){
+                echo "La taula ". $taula ." està plena.<br>";
             }
             else{
-                echo "té X comensals.<br>";
+                echo "La taula ". $taula ." té ". $taulesRestaurant[$taula] . " comensals.<br>";
+            }
+        }
+
+        echo "<h1>HOTEL</h1>";
+
+        $hotel = array();
+
+        for($planta = 0; $planta < 5; $planta++){
+            for($hab = 0; $hab < 10; $hab++){
+                //ASSIGNO CLIENTS SEGONS PLANTA I HABITACIO
+                $hotel[$planta][$hab] = rand(0,4);
+
+                //echo "ets a la planta " . $planta . " a la habitacio " . $hab . "<br>";
+
+                if($hotel[$planta][$hab] == 0){
+                    echo "La habitació ". $hab ." de la planta " . $planta . " està buida.<br>";
+                }
+                elseif($hotel[$planta][$hab] == 4){
+                    echo "La habitació ". $hab ." de la planta " . $planta . " està plena.<br>";
+                }
+                else{
+                    echo "La habitació ". $hab . " de la planta " . $planta ." té ". $hotel[$planta][$hab] . " comensals.<br>";
+                }
             }
         }
 
